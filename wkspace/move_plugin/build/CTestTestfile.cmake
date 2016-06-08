fb936cb4e07f179f6a1995fae9a31deaa4746cbc
Markus <m4k.join@outlook.com>
2016-06-08 17:35:32 +0100

    gazebo plugin -> rospackage
    added gazebo plugin as rospackage
    - testphase -
    just echo hello world currently

---
+++ wkspace/move_plugin/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubu/Gazebo_Strategy_Plugin/wkspace/move_plugin/src
+# Build directory: /home/ubu/Gazebo_Strategy_Plugin/wkspace/move_plugin/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
